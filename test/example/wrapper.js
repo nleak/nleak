e927af68552af6c9ece327346d64a36f11c3de25
chenxizh <foreseazhang@gmail.com>
2022-11-06 16:37:06 -0800

    feat: add rewriting to wrapper and successfully got the rewritten guest app

--- test/example/wrapper.js
+++ test/example/wrapper.js
@@ -1,21 +1,35 @@
 const Module = require('module');
 const fs = require('fs')
 const originalRequire = Module.prototype.require;
+const exposeClosureState = require("./rewriting/closure_state_transform").exposeClosureState;
 
+// Modify the require function to rewrite the guest app
 Module.prototype.require = function(){
   const fileName = arguments['0'];
   try {
     const filePath = require.resolve(fileName);
     if (!fs.existsSync(filePath)) {
-        throw 'Non-local module required.';
+      throw "Non-local module required.";
     }
     const srcStr = fs.readFileSync(filePath).toString();
+
+    // rewriting starts here
+    const rewritten = exposeClosureState("guest_app_rewritten.js", srcStr);
+    fs.writeFile("./guest_app_rewritten.log", rewritten, function (err) {
+      if (err) return console.log(err);
+      console.log(
+        "[DEBUG] saved rewritten guest app to guest_app_rewritten.log"
+      );
+    });
+
+    // export to file
     const constructor = new module.constructor();
-    constructor._compile(srcStr.replace('memory leaking...', 'no leaking...'), fileName);
+    constructor._compile(rewritten, fileName);
     return constructor.exports;
   } catch {
     return originalRequire.apply(this, arguments);
   }
 };
 
-const guestApp = require('./guest_app.js');
+// run the guest app after rewriting.
+require('./guest_app.js');
