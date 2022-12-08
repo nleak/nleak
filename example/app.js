c4bb593fe705d6c993b3e78c85da487985097d9c
chenxizh <foreseazhang@gmail.com>
2022-12-08 10:13:16 -0800

    feat: add example config and app

---
+++ example/app.js
@@ -0,0 +1,38 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 2333;
+
+//add callable methods by other script aka wrapper.js
+
+global.LEAKOBJ = {};
+var power = 2;
+function leaking() {
+    var top = Math.pow(2, power);
+    power++;
+    for (var j = 0; j < top; j++) {
+      LEAKOBJ[Math.random()] = Math.random();
+    }
+    console.log("memory leaking...");
+}
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/leak" && req.method === "GET") {
+    leaking();
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write("leaking() done");
+    res.end();
+  } else if (req.url === "/" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("hello from sample_app.js");
+    res.end();
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/, with API /leak`);
+});
